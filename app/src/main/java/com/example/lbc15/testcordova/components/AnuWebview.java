083f9863d90df7f0ce2d7ec3089a4f23957e51db
andy <1585638808@qq.com>
2017-08-11 21:29:53 +0800

    init:battery:

---
+++ app/src/main/java/com/example/lbc15/testcordova/components/AnuWebview.java
@@ -0,0 +1,15 @@
+package com.example.lbc15.testcordova.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+/**
+ * Created by lbc15 on 2017/8/11.
+ */
+
+public class AnuWebview extends WebView {
+    public AnuWebview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
